987514ce4c1397aae76ac2590357c324d7d89d9d
Lucas <lucasgenial@hotmail.com>
2020-10-20 15:20:47 -0300

    Modificações Dashboard

--- src/main/resources/static/assets/js/grafico-ocorrencia.js
+++ src/main/resources/static/assets/js/grafico-ocorrencia.js
@@ -1,274 +1,62 @@
+//Graifico de Cidades
+var grafico1 = echarts.init(document.getElementById('grf_1'));
+
+//Grafico de Horas
+var grafico2 = echarts.init(document.getElementById('grf_2'));
+
+
 function geradorDeGraficosDashboard(id) {
-	/*
-	 * Inicio Configuração Gráfico 3
-	 */
-	var grafico1 = echarts.init(document.getElementById('grf_1'));
-
-	// Abre o LOADING
-	grafico1.showLoading();
-
-	$.getJSON("/admin/cadastros/lista/ocorrencias/abrangencia/" + id, function(dados) {
-		// FINALIZA O LADING
-		grafico1.hideLoading();
-
-		var dados2 = genData(dados);
-		
-		// RECEBE AS OPÇOES DO GRAFICO
-		opcoes_grafico1 = {
-			tooltip : {
-				trigger : 'axis'
-			},
-			grid : {
-				bottom : '10%'
-			},
-			dataZoom : [ {
-				type : 'inside'
-			} ],
-			xAxis : {
-				type : 'value',
-				splitNumber : 5,
-				axisLine : {
-					show : true
-				},
-				axisTick : {
-					show : true
-				},
-				axisLabel : {
-					textStyle : {
-						color : '#000'
-					}
-				}
-			},
-			yAxis : {
-				data : dados2.nomes,
-				axisLabel : {
-					inside : true,
-					textStyle : {
-						color : '#000',
-						fontWeight: 'bold',
-						fontSize: 14
-					}
-				},
-				z : 25
-			},
-			series : [ {
-				type : 'bar',
-				data : dados2.valores,
-			} ]
-		};
-		
-		// RECEBE AS OPÇOES DO GRAFICO
-		grafico1.setOption(opcoes_grafico1);
-		
-	});
 	
 	
-	function genData(data) {
-		// Recebe os nomes das tipificações
-		var nomes = data.nome;
-
-		// Transforma em um json de Arrays de Objetos
-		var valores = [];
-
-		// Fica com os campos que foram selecionados
-		var selected = {};
-		
-		for (var i = 0; i <= nomes.length; i++) {
-			valores.push({
-				value : data.quantidade[i],
-				itemStyle : {
-					color : data.cores[i]
-				}
-			});
-		}
-
-		return {
-			nomes : nomes,
-			valores : valores
-		}
-	}
-
-	/*
-	 * Inicio Configurações Grafico Ocorrencias por Hora
-	 */
-	var grafico2 = echarts.init(document.getElementById('grf_2'));
-
-	grafico2.showLoading();
-
-	$.getJSON("/admin/cadastros/lista/ocorrencias/hora/" + id, function(dados) {
-
-		grafico2.hideLoading();
-
-		opcoes_grafico2 = {
-			// Make gradient line here
-			visualMap : {
-				show : true,
-				type : 'continuous',
-				seriesIndex : 0,
-				min : 0,
-				max : 10
-			},
-			tooltip : {
-				trigger : 'axis'
-			},
-			xAxis : {
-				data : dados.nome
-			},
-			yAxis : {
-				splitLine : {
-					show : false
-				}
-			},
-			grid : {
-				bottom : '10%'
-			},
-			series : {
-				type : 'line',
-				showSymbol : true,
-				data : dados.quantidade
-			}
-		};
-		
-		// RECEBE AS OPÇOES DO GRAFICO
-		grafico2.setOption(opcoes_grafico2);
-
-	});	
+	geraGraficoTipificacao(id);
 	
-	$.getJSON("/admin/cadastros/lista/ocorrencias/" + id, function(dados1) {
-		var nomes = dados1.nomes;
-		var html = "";
-
-		for (var i = 0; i < dados1.nome.length; i++) {
-			var nome = dados1.nome[i];
-			var qtd = dados1.quantidade[i];
-			var perc = Number(dados1.percentual[i]);
-			var ativo = "";
-			
-			if(i==1){
-				var ativo = "active";
-			}
-		
-		html += '<div class="carousel-item col-6 '+ ativo +' " >'+
-
-					'<div class="card">'+
-						'<div class="card-body">'+
-							'<div style="height: 40px;">'+
-							'<h6 class="card-title text-center">'+ nome +'</h6>'+
-							'</div>'+
-							'<div class="row">'+
-								'<div class="d-flex no-block align-items-center">'+
-									'<div class="col-9">'+
-							   			'<div class="chart cart-pie-1" data-percent="'+ perc.toFixed(2) +'"> <span class="percent">'+ perc.toFixed(2) +'</span> </div>'+
-							   		'</div>'+
-									'<div class="col-12 text-center">'+
-									    '<h1 class="text-success">'+ qtd +'</h1>'+
-								    '</div>'+
-							    '</div>'+
-							 '</div>'+
-						 '</div>'+
-		              '</div>'+
-		            '</div>';
-		}
-		
-		$("#painel-itens").append(html).before(function(){
-			var EasyPieChart = function() {};
-			
-		    EasyPieChart.prototype.init = function() {
-				$('.cart-pie-1').easyPieChart({
-					easing: 'easeOutBounce',
-					barColor : '#13dafe',
-					lineWidth: 7,
-					animate: 1000,
-			        lineCap: 'square',
-			        trackColor: '#e5e5e5',
-					onStep: function(from, to, percent) {
-						$(this.el).find('.percent').text(percent.toFixed(2));
-					}
-				});
-		    };
-		    
-		    //init
-		    $.EasyPieChart = new EasyPieChart, $.EasyPieChart.Constructor = EasyPieChart
-		    $.EasyPieChart.init()
-		});
-	});
-		
-		
-		//Configura o OWL Carousel
-//		var painel1 = $('#apresentaPorClassificacao');
-//		
-//		painel1.owlCarousel({
-//			items:6,
-//		    loop:true,
-//		    margin:30,
-//		    autoplay:true,
-//		    dots: false,
-//		    autoplayTimeout:3000,
-//		    autoplayHoverPause:true,
-//		    owl2row: true, // enable plugin
-//	        owl2rowTarget: 'item',    // class for items in carousel div
-//	        owl2rowContainer: 'owl2row-item', // class for items container
-//	        owl2rowDirection: 'utd', // ltr : directions
-//		    responsive : {
-//			    0 : {
-//			    	items:1,
-//			    },
-//			    // ponto de interrupção de 480
-//			    576 : {
-//			    	items:2,
-//			    },
-//			    // ponto de interrupção de 768
-//			    768 : {
-//			    	items:3,
-//			    },
-//			    // ponto de interrupção de 768
-//			    992 : {
-//			    	items:4,
-//			    },
-//			    // ponto de interrupção de 768
-//			    1200 : {
-//			    	items:5,
-//			    	owl2row: false,
-//			    }
-//		    }
-//		});
-
+	geraGraficoCidades(id);
+	
+	geraGraficoHora(id);
+	
+	geraGraficoStatus(id);
 	
 	//Apaga painel 1
-	$("#painel-1").toggle();
-	
-	$.getJSON("/admin/cadastros/lista/ocorrencia/status/" + id, function(dados1) {
-		var coresStatus = [ '#00B23D', '#1E8DB2', '#FF9540', '#19A2FF' , '#89998C', '#FF1E17'];
-		var content = "";
-		var nomes = dados1.nomes;
-		
-		for (var i = 0; i < dados1.nome.length; i++) {
-		
-			var nome = dados1.nome[i];
-			var qtd = dados1.quantidade[i];
+	$("#painel-2").toggle();
+}
 
-			content += '<div class="item">'+
+//Verifica caso a tela é Redimensionada
+$(window).on('resize', function(){
+	grafico1.resize();
+	grafico2.resize();
+});
+
+function geraGraficoStatus(id){
+	/*
+	 * Inicio Configurações Grafico OCORRÊNCIAS POR STATUS
+	 */	
+	$.getJSON("/admin/cadastros/lista/ocorrencia/status/" + id, function(dados) {
+		var coresStatus = [ '#00B23D', '#1E8DB2', '#FF9540', '#19A2FF' , '#89998C', '#FF1E17'];
+		var content     = "";
+		var nomes       = dados.nomes;
+
+		for (var i = 0; i < dados.nome.length; i++) {
+
+			var nome = dados.nome[i];
+			var qtd = dados.quantidade[i];
+
+			content += '<div class="item" style="min-height: 100%">'+
 			'<div class="card text-center" style="background:'+ coresStatus[i] +'; align-center ">'+
 				'<div class="card-body">'+
 					'<h5 class="card-title">'+ nome +'</h5>'+
 					'<div class="row text-center">'+
-						
 							'<div class="col-12" >'+
 							    '<h1 class="card-title">'+ qtd +'</h1>'+
 						    '</div>'+
-					   
 					 '</div>'+
 				 '</div>'+
               '</div>'+
             '</div>';
 		}
-		
+
 		$("#apresentacaoPorStatus").append(content);
 		
-		//Configura o OWL Carousel
-		var painel2 = $('#apresentacaoPorStatus');
-		
-		painel2.owlCarousel({
+		$("#apresentacaoPorStatus").owlCarousel({
 		    loop:false,
 		    margin:10,
 		    autoplay:true,
@@ -300,6 +88,235 @@ function geradorDeGraficosDashboard(id) {
 			    	items:3,
 			    }
 		    }
-		});	
+		});
+	});
+};
+
+function geraGraficoHora(id){
+	/*
+	 * Inicio Configurações Grafico OCORRÊNCIAS POR HORA
+	 */
+	grafico2.showLoading();
+
+	$.getJSON("/admin/cadastros/lista/ocorrencias/hora/" + id, function(dados) {
+
+		grafico2.hideLoading();
+
+		opcoes_grafico2 = {
+			// Make gradient line here
+			visualMap : {
+				show : true,
+				type : 'continuous',
+				seriesIndex : 0,
+				min : 0,
+				max : 10
+			},
+			tooltip : {
+				trigger : 'axis'
+			},
+			xAxis : {
+				data : dados.nome
+			},
+			yAxis : {
+				splitLine : {
+					show : false
+				}
+			},
+			grid: {
+			    top: '6',
+			    right: '0',
+			    bottom: '17',
+			    left: '25',
+			},
+//			grid : {
+//				bottom : '10%'
+//			},
+			series : {
+				type : 'line',
+				showSymbol : true,
+				data : dados.quantidade
+			}
+		};
+		
+		// RECEBE AS OPÇOES DO GRÁFICO
+		grafico2.setOption(opcoes_grafico2);
+
+	});
+}
+
+function geraGraficoCidades(id){
+	/*
+	 * Início Configuração Gráfico de OCORRÊNCIAS POR CIDADES
+	 */
+
+	// Abre o LOADING
+	grafico1.showLoading();
+
+	$.getJSON("/admin/cadastros/lista/ocorrencias/abrangencia/" + id, function(dados) {
+		// FINALIZA O LADING
+		grafico1.hideLoading();
+
+		var dadosTratados = genData(dados);
+		
+		// RECEBE AS OPÇOES DO GRAFICO
+		opcoes_grafico1 = {
+			tooltip : {
+				trigger : 'axis'
+			},
+			grid: {
+			    top: '6',
+			    right: '0',
+			    bottom: '17',
+			    left: '25',
+			},
+			dataZoom : [ {
+				type : 'inside'
+			} ],
+			xAxis : {
+				type : 'value',
+				splitNumber : 5,
+				axisLine : {
+					show : true
+				},
+				axisTick : {
+					show : true
+				},
+				axisLabel : {
+					textStyle : {
+						color : '#000'
+					}
+				}
+			},
+			yAxis : {
+				data : dadosTratados.nomes,
+				axisLabel : {
+					inside : true,
+					textStyle : {
+						color : '#000',
+						fontWeight: 'bold',
+						fontSize: 14
+					}
+				},
+				z : 25
+			},
+			series : [ {
+				type : 'bar',
+				data : dadosTratados.valores,
+			} ]
+		};
+		
+		// RECEBE AS OPÇÕES DO GRÁFICO
+		grafico1.setOption(opcoes_grafico1);
+		
+	});
+	
+	
+	function genData(data) {
+		// Recebe os nomes das tipificações
+		var nomes = data.nome;
+
+		// Transforma em um json de Arrays de Objetos
+		var valores = [];
+
+		// Fica com os campos que foram selecionados
+		var selected = {};
+		
+		for (var i = 0; i <= nomes.length; i++) {
+			valores.push({
+				value : data.quantidade[i],
+				itemStyle : {
+					color : data.cores[i]
+				}
+			});
+		}
+
+		return {
+			nomes : nomes,
+			valores : valores
+		}
+	}
+	
+}
+
+function geraGraficoTipificacao(id){
+	/*
+	 * Inicio Configurações Grafico OCORRÊNCIAS POR TIPIFICAÇÃO 
+	 */
+	$.getJSON("/admin/cadastros/lista/ocorrencias/" + id, function(dados) {
+		var nomes = dados.nomes;
+		var html = "";
+
+		for (var i = 0; i < dados.nome.length; i++) {
+			var nome  = dados.nome[i];
+			var qtd   = dados.quantidade[i];
+			var perc  = Number(dados.percentual[i]);
+			var ativo = "";
+			
+			if(i == 1){
+				var ativo = "active";
+			}
+
+			html += '<div class="card" style="margin-top:0px">'+
+						'<div class="card-body">'+
+							'<div style="height: 40px;">'+
+								'<h6 class="card-title text-center">'+ nome +'</h6>'+
+							'</div>'+
+							'<div class="row">'+
+								'<div class="d-flex no-block align-items-center">'+
+									'<div class="col-9">'+
+							   			'<div class="chart cart-pie-1" data-percent="'+ perc.toFixed(2) +'"> <span class="percent">'+ perc.toFixed(2) +'</span> </div>'+
+							   		'</div>'+
+									'<div class="col-12 text-center">'+
+									    '<h1 class="text-success">'+ qtd +'</h1>'+
+								    '</div>'+
+							    '</div>'+
+							 '</div>'+
+						 '</div>'+
+		              '</div>';
+		}
+		
+		$("#apresentaPorClassificacao").append(html);
+				
+		$(".cart-pie-1").easyPieChart({
+	        easing: 'easeOutBounce',
+			barColor : '#13dafe',
+			lineWidth: 7,
+			animate: 500,
+	        lineCap: 'square',
+	        trackColor: '#e5e5e5',
+			onStep: function(from, to, percent) {
+				$(this.el).find('.percent').text(percent.toFixed(2));
+			}
+	    });
+
+		$('#apresentaPorClassificacao').owlCarousel({
+			loop:true,
+		    margin:20,
+			dots:false,
+		    nav:false,
+			autoplay:true,
+    		autoplayTimeout:3000,
+    		autoplayHoverPause:true,
+		    responsive : {
+			    0 : {
+			    	items:1,
+			    },
+			    1150 : {
+			    	items:3,
+			    },
+			    1200 : {
+			    	items:4,
+			    	owl2row: false,
+			    },
+				1400 : {
+			    	items:4,
+			    	owl2row: false,
+			    },
+				1600 : {
+			    	items:5,
+			    	owl2row: false,
+			    }
+		    }
+		});
 	});
 }
--- src/main/resources/static/assets/js/refreshDashboard.js
+++ src/main/resources/static/assets/js/refreshDashboard.js
@@ -1,56 +1,92 @@
-setTimeout('refreshDashBoard()', 180000);
+$(document).ready(function() {
+	setInterval('refreshPage()', 180000);
 
-setTimeout(function(){
-	$("#painel-1").toggle();
-	$("#painel-2").toggle();
-	
-}, 90000);
-
-window.addEventListener('resize', function(){
-	refreshDashBoard();
+	 setInterval(function(){
+		$("#painel-2").toggle();
+		$("#painel-1").toggle();
+	}, 6000);	
 });
 
-function refreshDashBoard() {	
-	document.getElementById('grupo').innerHTML = `
-			<div class="row">
-				<div class="col-12">
-					<div id="apresentaPorClassificacao" class="row owl-carousel owl-theme"></div>
-				</div>
-			</div>
+// Criado um Observador de mutações do código para gerar um refresh da tela
+// caso o Menu fosse fechado
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+	if($('body').hasClass("mini-sidebar")){
+		//geraGraficoTipificacao(codigo);
+		$('#apresentaPorClassificacao').owlCarousel().trigger('refresh.owl.carousel');
+		$('#apresentacaoPorStatus').owlCarousel().trigger('refresh.owl.carousel');
+		grafico1.resize();
+		grafico2.resize();
+	}else{
+		$('#apresentaPorClassificacao').owlCarousel().trigger('refresh.owl.carousel');
+		$('#apresentacaoPorStatus').owlCarousel().trigger('refresh.owl.carousel');
+		grafico1.resize();
+		grafico2.resize();
+	}
+  });
+});
 
-			<div class="row">
-				<div id="painel-3" class="col-6">
-					<div class="ribbon-wrapper card">
-						<div class="ribbon ribbon-info">OCORRÊNCIAS - CIDADES</div>
-						<div id="grf_1" style="width: 100%; min-height: 400px;"></div>
-					</div>
-				</div>
-				
-				<div id="painel-2" class="col-6">
-					<div class="ribbon-wrapper card">
-						<div class="ribbon ribbon-info">OCORRÊNCIAS - STATUS</div>
-						<div id="apresentacaoPorStatus" class="owl-carousel owl-theme" style="width: 100%; min-height: 400px"></div>
-					</div>
-				</div>
-				
-				<div id="painel-1" class="col-6">
-					<div class="ribbon-wrapper card">
-						<div class="ribbon ribbon-info">OCORRÊNCIAS - HORA</div>
-						<div id="grf_2" style="width: 100%; min-height: 400px"></div>						
-					</div>
-				</div>
-				</div>				
-			`
-	geradorDeGraficosDashboard(document.getElementById("identificador").value);
-	setTimeout('refreshDashBoard()', 180000);
+observer.observe(document.body, {
+  attributes: true
+});
 
-	setTimeout(function(){
-		$("#painel-1").toggle();
-		$("#painel-2").toggle();
-		
-	}, 90000);
+function refreshPage() { 
+	location.reload(1); 
+}
+//
+//var te2 = setTimeout(function(){
+//	$("#painel-1").toggle();
+//	$("#painel-2").toggle();
+//}, 90000);
+//
+//window.addEventListener('resize', function(){
+//	document.location.reload();
+////	window.location.reload();
+////	refreshDashBoard();
+//});
 
-	window.addEventListener('resize', function(){
-		refreshDashBoard();
-	});
-};
+
+//function refreshDashBoard() {	
+//	document.getElementById('grupo').innerHTML = `
+//			<div class="row">
+//				<div class="col-12">
+//					<div id="apresentaPorClassificacao" class="row owl-carousel owl-theme"></div>
+//				</div>
+//			</div>
+//
+//			<div class="row">
+//				<div id="painel-3" class="col-6">
+//					<div class="ribbon-wrapper card">
+//						<div class="ribbon ribbon-info">OCORRÊNCIAS - CIDADES</div>
+//						<div id="grf_1" style="width: 100%; min-height: 400px;"></div>
+//					</div>
+//				</div>
+//				
+//				<div id="painel-2" class="col-6">
+//					<div class="ribbon-wrapper card">
+//						<div class="ribbon ribbon-info">OCORRÊNCIAS - STATUS</div>
+//						<div id="apresentacaoPorStatus" class="owl-carousel owl-theme" style="width: 100%; min-height: 400px"></div>
+//					</div>
+//				</div>
+//				
+//				<div id="painel-1" class="col-6">
+//					<div class="ribbon-wrapper card">
+//						<div class="ribbon ribbon-info">OCORRÊNCIAS - HORA</div>
+//						<div id="grf_2" style="width: 100%; min-height: 400px"></div>						
+//					</div>
+//				</div>
+//				</div>				
+//			`
+//	geradorDeGraficosDashboard(document.getElementById("identificador").value);
+//	setTimeout('refreshDashBoard()', 180000);
+//
+//	setTimeout(function(){
+//		$("#painel-1").toggle();
+//		$("#painel-2").toggle();
+//		
+//	}, 90000);
+//
+//	window.addEventListener('resize', function(){
+//		refreshDashBoard();
+//	});
+//};
