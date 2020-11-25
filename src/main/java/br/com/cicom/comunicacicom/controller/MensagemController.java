0ae890227d4a972229fc20aedf79b9db1eb6096a
Lucas <lucasgenial@hotmail.com>
2020-11-25 18:20:04 -0300

    trabalhos dia 25-11-2020

--- src/main/java/br/com/cicom/comunicacicom/controller/MensagemController.java
+++ src/main/java/br/com/cicom/comunicacicom/controller/MensagemController.java
@@ -1,33 +1,34 @@
 package br.com.cicom.comunicacicom.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cicom.comunicacicom.DSPrimary.DTO.seguranca.GrupoDTO;
 import br.com.cicom.comunicacicom.DSPrimary.DTO.seguranca.UsuarioDTO;
 import br.com.cicom.comunicacicom.DSPrimary.DTO.sisNotificacao.MensagemDTO;
 import br.com.cicom.comunicacicom.DSPrimary.model.seguranca.Grupo;
 import br.com.cicom.comunicacicom.DSPrimary.model.seguranca.Usuario;
-import br.com.cicom.comunicacicom.DSPrimary.model.sisMensagem.Mensagem;
 import br.com.cicom.comunicacicom.DSPrimary.service.seguranca.GrupoService;
 import br.com.cicom.comunicacicom.DSPrimary.service.seguranca.UsuarioService;
 
@@ -48,37 +49,61 @@ public class MensagemController {
 	public List<MensagemDTO> listarMensagens(ModelAndView mv) {
 		return null;
 	}
+	
+	@RequestMapping(value = "/admin/mensagens/nova")
+	public ModelAndView novaMensagensPage(ModelAndView model, MensagemDTO mensagem) {
+		//Model model = new ModelAndView("/fragmentos/mensagem/novaMensagem");
 
-	@RequestMapping(value = "**/cadastrarMensagem", method = { RequestMethod.POST})
-	public String cadastrarMensagem(Model model, @Valid MensagemDTO mensagem, BindingResult result) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		Usuario user = servicoUsuario.buscaPeloLogin(auth.getName());
 		
-		Usuario user = servicoUsuario.buscaPeloLogin(SecurityContextHolder.getContext().getAuthentication().getName());
-		model.addAttribute("usuario", user);
+		model.addObject("usuario", user);	
 		
-		if (result.hasErrors()) {
-			
-			System.out.println("ERROR OOOOOO 1");
-			
-//			model.addAttribute("org.springframework.validation.BindingResult.mensagem", result);
-//			model.addAttribute("tituloPagina", "ComunicaCICOM - Nova Mesa");		
-//			model.addAttribute("novaMensagem", mensagem);
-//			model.addAttribute("usuario", user);
-//			model.addAttribute("criador", user);
-//			
-//			model.addAttribute("listaUsuarios", servicoUsuario.buscarPorEstabelecimentos(user.getEstabelecimento())
-//					.stream().filter(e -> e.getServidor()!=null && !e.getServidor().getNome().equalsIgnoreCase("BOMBEIRO MILITAR"))
-//					.collect(Collectors.toList()));
-//			
-//			model.addAttribute("tituloPagina", "ComunicaCICOM - Nova Mensagem");
-			
-			return "/fragmentos/mensagem/novaMensagem";
+//		MensagemDTO mensagem = new MensagemDTO();
+		mensagem.setEmissor(user.getId());
+				
+		// Adiciona uma nova mensagem na view
+		// Verifica se já foi passado a ocorrência
+		if (!model.getModel().containsKey("novaMensagem")) {
+			mensagem.setDataCriacao(LocalDateTime.now());
+			model.addObject("novaMensagem", mensagem);
 		}
 		
-		System.out.println(mensagem);
-//		System.out.println(listaUsuarios);
+		// Adiciona uma lista de Grupos
+		model.addObject("listaGrupos", servicoGrupo.listarTodos().stream().collect(Collectors.toList()));
+	
+		// Adiciona uma lista de Usuários por estabelecimento do Usuário Logado.
+		model.addObject("listaUsuarios", servicoUsuario.buscarPorEstabelecimentos(user.getEstabelecimento())
+				.stream().filter(e -> e.getServidor()!=null && !e.getServidor().getNome().equalsIgnoreCase("BOMBEIRO MILITAR"))
+				.collect(Collectors.toList()));
 		
-		return "redirect:/admin/mensagens/entrada";
+		model.addObject("tituloPagina", "ComunicaCICOM - Nova Mensagem");
+
+		model.setViewName("/fragmentos/mensagem/novaMensagem");
 		
+		return model;
+	}
+
+
+	@RequestMapping(value = "**/cadastrarMensagem", method = { RequestMethod.POST})
+	public String cadastrarMensagem(@Valid MensagemDTO mensagem, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//
+//		Usuario user = servicoUsuario.buscaPeloLogin(auth.getName());
+				
+		if (result.hasErrors()) {
+			System.out.println("ERRO!");
+			System.out.println(result.toString());
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.mensagemDTO", result);
+			return "redirect:/admin/mensagens/nova";
+		} else{
+			System.out.println(mensagem);
+//			System.out.println(listaUsuarios);
+			
+			return "redirect:/admin/mensagens/entrada";
+		}
 	}
 
 	@ResponseBody
@@ -134,4 +159,6 @@ public class MensagemController {
 	public Usuario converterParaUsuario(UsuarioDTO usuarioDTO) {
 		return modelMapper.map(usuarioDTO, Usuario.class);
 	}
+	
+	
 }
--- src/main/java/br/com/cicom/comunicacicom/controller/MenuNotificacoes.java
+++ src/main/java/br/com/cicom/comunicacicom/controller/MenuNotificacoes.java
@@ -1,12 +1,8 @@
 package br.com.cicom.comunicacicom.controller;
 
-import java.time.LocalDateTime;
-import java.util.stream.Collectors;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,13 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.cicom.comunicacicom.DSPrimary.DTO.seguranca.GrupoDTO;
-import br.com.cicom.comunicacicom.DSPrimary.DTO.seguranca.UsuarioDTO;
-import br.com.cicom.comunicacicom.DSPrimary.DTO.sisNotificacao.MensagemDTO;
-import br.com.cicom.comunicacicom.DSPrimary.model.seguranca.Grupo;
 import br.com.cicom.comunicacicom.DSPrimary.model.seguranca.Usuario;
-import br.com.cicom.comunicacicom.DSPrimary.model.sisMensagem.Mensagem;
-import br.com.cicom.comunicacicom.DSPrimary.service.seguranca.GrupoService;
 import br.com.cicom.comunicacicom.DSPrimary.service.seguranca.UsuarioService;
 
 @Controller
@@ -28,12 +18,6 @@ public class MenuNotificacoes {
 
 	@Autowired
 	private UsuarioService servicoUsuario;
-
-	@Autowired
-	private GrupoService servicoGrupo;
-	
-	@Autowired
-    private ModelMapper modelMapper;
 
 	@RequestMapping(value = "/admin/mensagens/entrada")
 	public String entradaNotificacaoPage(Model model, HttpSession session, HttpServletRequest req) {
@@ -57,64 +41,5 @@ public class MenuNotificacoes {
 		model.addAttribute("tituloPagina", "ComunicaCICOM - Mensagens");
 
 		return "/fragmentos/mensagem/listarMensagem";
-	}
-
-	@RequestMapping(value = "/admin/mensagens/nova")
-	public String novaMensagensPage(Model model) {
-		//Model model = new ModelAndView("/fragmentos/mensagem/novaMensagem");
-
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-		Usuario user = servicoUsuario.buscaPeloLogin(auth.getName());
-		model.addAttribute("usuario", user);
-		
-		if (user == null) {
-			model.addAttribute("erro", true);
-			
-			model.addAttribute("mensagem", "Foi encontrado um erro!");
-
-			return "/fragmentos/mensagem/novaMensagem";
-		}
-				
-		model.addAttribute("usuario", user);
-		
-		MensagemDTO mensagem = new MensagemDTO();
-		mensagem.setEmissor(user.getId());
-//		System.out.println(mensagem.getEmissor());
-				
-		// Adiciona uma nova mensagem na view
-		// Verifica se já foi passado a ocorrência
-		if (!model.containsAttribute("novaMensagem")) {
-			mensagem.setDataCriacao(LocalDateTime.now());
-			model.addAttribute("novaMensagem", mensagem);
-		}
-		
-		// Adiciona uma lista de Grupos
-		model.addAttribute("listaGrupos", servicoGrupo.listarTodos().stream().collect(Collectors.toList()));
-	
-		// Adiciona uma lista de Usuários por estabelecimento do Usuário Logado.
-		model.addAttribute("listaUsuarios", servicoUsuario.buscarPorEstabelecimentos(user.getEstabelecimento())
-				.stream().filter(e -> e.getServidor()!=null && !e.getServidor().getNome().equalsIgnoreCase("BOMBEIRO MILITAR"))
-				.collect(Collectors.toList()));
-		
-		model.addAttribute("tituloPagina", "ComunicaCICOM - Nova Mensagem");
-
-		return "/fragmentos/mensagem/novaMensagem";
-	}
-
-	public GrupoDTO converterParaGrupoDTO(Grupo grupo) {
-		return modelMapper.map(grupo, GrupoDTO.class);
-	}
-
-	public Grupo converterParaGrupo(GrupoDTO grupoDTO) {
-		return modelMapper.map(grupoDTO, Grupo.class);
-	}
-
-	public UsuarioDTO user(Usuario usuario) {
-		return modelMapper.map(usuario, UsuarioDTO.class);
-	}
-
-	public Usuario converterParaUsuario(UsuarioDTO usuarioDTO) {
-		return modelMapper.map(usuarioDTO, Usuario.class);
 	}
 }
